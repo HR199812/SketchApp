7b50f1f01dec2514f42728612fd613fc12fe8aec
Hritwik Agarwal <39455725+HR199812@users.noreply.github.com>
2021-07-12 19:18:06 +0530

    Initialised drawing app
    Main page with basic functionalities of creating a new document and changing canvas color.

---
+++ app.js
@@ -0,0 +1,296 @@
+//Initialising Canvas and it's context for operations on canvas
+var canvas = document.querySelector('.DrawingPage');
+var ctx = canvas.getContext("2d");
+
+
+var drawingPad = document.querySelector('.DrawingPad');
+var documentColorCode = document.querySelector('.DocumentColorPicker');
+var fullScreen = document.querySelector('.FullScreen');
+var normalScreen = document.querySelector('.NormalScreenButton');
+
+
+//Hide Drawing Tools
+function HideDrawingTools() {
+    anime({
+        targets: '.DrawMenu',
+        translateY: [
+            { value: 0, duration: 500 },
+            { value: -90, duration: 500 },
+        ]
+    });
+}
+
+
+
+// Change Canvas Color
+documentColorCode.addEventListener("input", () => {
+    // drawingPad.style.backgroundColor = `${documentColorCode.value}`;
+
+    ctx.fillStyle = `${documentColorCode.value}`;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+});
+
+// Changing Canvas Color back to white when clearing canvas
+document.querySelector('.ClearCanvas').addEventListener('click', () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawingPad.style.backgroundColor = "white";
+})
+
+
+//Resize the canvas on window resize event
+window.addEventListener('load', () => {
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+});
+
+document.querySelector(".DrawingPage").addEventListener("click", (event) => {
+    getPosition(event);
+    document.addEventListener('mousedown', startPainting);
+    document.addEventListener('mouseup', stopPainting);
+    document.addEventListener('mousemove', sketch);
+});
+
+
+//Full Screen Mode
+function FullScreen() {
+
+    if (document.body.requestFullscreen) {
+        document.body.requestFullscreen();
+    } else if (document.body.webkitRequestFullscreen) { /* Safari */
+        document.body.webkitRequestFullscreen();
+    } else if (document.body.msRequestFullscreen) { /* IE11 */
+        document.body.msRequestFullscreen();
+    }
+
+    fullScreen.style.display = 'none';
+    normalScreen.style.display = 'inline-block';
+}
+
+
+//Normal Screen Mode
+function NormalScreen() {
+
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+        document.msExitFullscreen();
+    }
+
+    fullScreen.style.display = 'inline-block';
+    normalScreen.style.display = 'none';
+}
+
+// The following functions toggle the flag to start
+// and stop drawing
+function startPainting(event) {
+    paint = true;
+    getPosition(event);
+}
+function stopPainting() {
+    paint = false;
+}
+
+
+//Sketch on the canvas as mouse is moved.
+function sketch(event) {
+    if (!paint) return;
+
+    ctx.beginPath();
+
+
+    ctx.lineWidth = strokeLine;
+    ctx.strokeStyle = `${pencilColor}`;
+
+    // Sets the end of the lines drawn
+    // to a round shape.
+    ctx.lineJoin = ctx.lineCap = 'round';
+
+    // The cursor to start drawing
+    // moves to this coordinate
+    ctx.moveTo(coord.x, coord.y);
+
+    // The position of the cursor
+    // gets updated as we move the
+    // mouse around.
+    getPosition(event);
+
+    // A line is traced from start
+    // coordinate to this coordinate
+    if (!isSpray) {
+        ctx.lineTo(coord.x, coord.y);
+    }
+
+    // Draws the line.
+    ctx.stroke();
+
+}
+
+let coord = { x: 0, y: 0 };
+let paint = false;
+
+//Getting coordinates of mouse
+function getPosition(event) {
+    coord.x = event.clientX - canvas.offsetLeft;
+    coord.y = event.clientY - canvas.offsetTop;
+}
+
+
+var localStream = 'none';
+//Following Functions are for camera access
+function OpenCameraCanvas() {
+
+    anime({
+        targets: '.CameraAccess',
+        scaleX: [
+            { value: 0, duration: 100, delay: 500 },
+            { value: 1, duration: 900 },
+        ]
+    });
+
+    document.querySelector('.MainMenu').style.visibility = 'hidden';
+
+    drawingPad.style.opacity = "0.9";
+
+    document.querySelector('.CameraAccess').style.visibility = 'visible';
+
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.msGetUserMedia ||
+        navigator.oGetUserMedia || navigator.mozGetUserMedia;
+
+    if (navigator.getUserMedia) {
+
+        navigator.mediaDevices.getUserMedia({ video: true }).then(
+            function (stream) {
+                localStream = stream;
+                video.srcObject = localStream;
+                video.play();
+            }
+        ).catch(function (error) {
+            console.log("Something went wrong!");
+        });
+
+    }
+
+}
+
+
+// Trigger photo take
+function SnapTaken() {
+
+    // cameraCanvas.style.filter = filterVal;
+
+    cameraCanvasContext.drawImage(video, 0, 0, 640, 480);
+
+    //Create img from canvas
+    const imgUrl = cameraCanvas.toDataURL('image/png');
+
+    photos.setAttribute('src', imgUrl);
+
+    // photos.style.filter = filterVal;
+
+}
+
+
+//Closing camera view and shutting down camera stream
+function CloseCameraAccessView() {
+
+    document.querySelector('.MainMenu').style.visibility = 'visible';
+
+    drawingPad.style.opacity = "1";
+
+    document.querySelector('.CameraAccess').style.visibility = 'hidden';
+    localStream.getTracks().forEach((track) => {
+        track.stop();
+    });
+}
+
+window.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+        CloseCameraAccessView();
+    }
+})
+
+
+// filters.addEventListener("change", (e) => {
+
+//     filterVal = e.target.value;
+//     video.style.filter = filterVal;
+
+//     e.preventDefault();
+// });
+
+//Calling Snap Taken functin on Click of Snap button
+document.getElementById('snap').addEventListener('click', SnapTaken);
+document.querySelector('.AddSnappedImageToCanvas').addEventListener('click', () => {
+
+    ctx.drawImage(photos, 90, 90, 350, 300);
+});
+
+//Function to read an existing Image from the system
+function readURL(e) {
+
+    var reader = new FileReader();
+    reader.onload = function (event) {
+        var img = new Image();
+        img.onload = function () {
+            // canvas.width = img.width;
+            // canvas.height = img.height;
+            ctx.drawImage(img, 90, 90, 350, 300);
+        }
+        img.src = event.target.result;
+    }
+    reader.readAsDataURL(e.files[0]);
+}
+
+
+//Adding text box to the view
+// function AddTextBox() {
+
+//     console.log("HI");
+//     // Create a new instace of Canvas
+//     let canvasFabricText = new fabric.Canvas("DrawingPage");
+
+//     // Create a new Textbox instance
+//     let text = new fabric.Textbox('Check',
+//         {
+//             fontFamily: 'arial black',
+//             left: 100,
+//             top: 100,
+//             objecttype: 'text'
+//         });
+
+
+//     // Render the Textbox on Canvas
+//     canvasFabricText.add(text);
+
+//     canvasFabricText = 'none';
+// }
+
+
+//SplashScreen function
+document.addEventListener('DOMContentLoaded', (e) => {
+    anime({
+        targets: '.MessageBox',
+        translateY: [
+            { value: 0, duration: 500 },
+            { value: -900, duration: 500 },
+        ]
+    });
+    anime({
+        targets: '.DrawMenu',
+        translateY: [
+            { value: 0, duration: 500 },
+            { value: -90, duration: 500 },
+        ]
+    });
+    setTimeout(() => {
+        document.querySelector('.splash').classList.add('display-none');
+    }, 4000);
+});
+
+
+//Function to make spray value true
+document.getElementById('SprayButton').addEventListener('click', () => {
+    isSpray = true;
+});

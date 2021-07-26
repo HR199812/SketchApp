d5171d0c71a5596a507e5843d25cbe02f840fe3e
Hritwik Agarwal <39455725+HR199812@users.noreply.github.com>
2021-07-26 19:36:19 +0530

    Add touch functionality V1.2

--- app.js
+++ app.js
@@ -111,13 +111,14 @@ document.querySelector(".DrawingPage").addEventListener("click", (event) => {
     document.addEventListener('mouseup', stopPainting);
     document.addEventListener('mousemove', sketch);
 });
-document.querySelector(".DrawingPage").addEventListener("touchstart", (event) => {
-    alert('HI');
+document.querySelector(".DrawingPage").addEventListener("touch", (event) => {
+    getPosition(event);
+    document.querySelector(".DrawingPage").addEventListener('touchstart', startPainting);
+    document.querySelector(".DrawingPage").addEventListener('touchend', stopPainting);
+    document.querySelector(".DrawingPage").addEventListener('touchmove', sketch);
 });
 
-// document.querySelector(".DrawingPage").addEventListener('touchstart', startPainting);
-// document.querySelector(".DrawingPage").addEventListener('touchend', stopPainting);
-// document.querySelector(".DrawingPage").addEventListener('touchmove', sketch);
+
 
 //Show Message Box
 function ShowMessageBox() {

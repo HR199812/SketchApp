5a987c544a50c33626592662886a6e3debf7756c
Hritwik Agarwal <hritwik72@gmail.com>
2025-05-24 14:18:01 +0530

    Fix: CSS

--- sketch-app-next/src/components/MessageBox.tsx
+++ sketch-app-next/src/components/MessageBox.tsx
@@ -9,25 +9,25 @@ export default function MessageBox({ DownloadBoard }: PropsTypes) {
 
   return (
     <div className="fixed inset-0 z-50 w-full h-full bg-gray-600/20 backdrop-blur-md flex justify-center items-center">
-      <div className="bg-[#a8d9e1] rounded-xl px-4 py-8">
+      <div className="bg-white border shadow-md border-1 border-black rounded-xl px-4 py-8">
         <p className="text-xl font-bold">All your Progress will get lost.</p>
         <br />
-        <p className="text-lg font-semibold">
+        <p className="text-md font-semibold">
           Download the current progress to save it.
         </p>
         <br />
         <div className="flex justify-between items-center px-4">
           <button
             onClick={() => DownloadBoard()}
-            className="font-bold px-4 py-2 border border-1 rounded-xl"
+            className="font-bold bg-black text-white px-4 py-2 border border-1 rounded-xl"
           >
-            Ok
+            Download
           </button>
           <button
             onClick={() => setShowMessageCard(false)}
-            className="font-bold px-4 py-2 border border-1 rounded-xl"
+            className="font-bold bg-black text-white px-4 py-2 border border-1 rounded-xl"
           >
-            Cancel
+            Ok
           </button>
         </div>
       </div>
